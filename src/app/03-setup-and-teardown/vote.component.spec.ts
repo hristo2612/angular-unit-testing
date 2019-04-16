09679dd12b0006c4567f5247fe20db7dba8a1df4
hristo2612 <hristoapps@gmail.com>
2019-04-16 16:36:25 +0300

    03 - Setup and Teardown - things learned:  1. beforeEach - called before each test - good for setup 2. afterEach - called after each test - good for cleanup 3. beforeAll - called before all tests in file 4. afterAll - called after all tests in file

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,27 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+
+  let component: VoteComponent;
+  
+  // executed before each test
+  beforeEach(() => {
+    // set up
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  // executed after each test - good for cleanup
+  afterEach(() => {
+    // tear down
+  });
+
+  it('should increment totalVotes when upvoted', () => {
+    component.upVote();
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    component.downVote();
+    expect(component.totalVotes).toBe(-1);
   });
 });
