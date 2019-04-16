ba27e4c25fc25f6e7ca0ee93d26c475ea3145797
hristo2612 <hristoapps@gmail.com>
2019-04-16 15:32:59 +0300

    Add karma-spec-reporter to karma.conf file

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -8,9 +8,10 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      // require('karma-jasmine-html-reporter'),
+      // require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-spec-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,7 +21,10 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['spec'], // progress, kjhtml - for defaul reporters
+    specReporter: {
+      maxLogLines: 10
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
