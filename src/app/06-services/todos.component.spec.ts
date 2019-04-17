a415b6d9f5532b9ff656a21d663f577025f29fa8
hristo2612 <hristoapps@gmail.com>
2019-04-17 15:43:50 +0300

    05 - services
    - delete method

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -63,4 +63,22 @@ describe('TodosComponent', () => {
     // expect the component message property to be the error returned from the observable
     expect(component.message).toBe(error);
   });
+
+  it('should call the server to delete a todo item if the user confirms dialog', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should NOT call the server to delete a todo item if the user cancels dialog', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalledWith(1);
+  });
 });
